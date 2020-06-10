9da8246361c245792a647ee93fd8239726aed8d9
shashirohith <shashirohith25@gmail.com>
2020-06-10 07:20:33 +0530

    Updates

---
+++ precache-manifest.40d947c074f030e3df2f0ee086eebe74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d012af34753a81ea1465b26bb0778ba",
+    "url": "/redux-movieinfo/index.html"
+  },
+  {
+    "revision": "76eb001c96fda556ccfa",
+    "url": "/redux-movieinfo/static/css/main.01690f44.chunk.css"
+  },
+  {
+    "revision": "f2b53d8b08fb18df7b8f",
+    "url": "/redux-movieinfo/static/js/2.5e9520d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-movieinfo/static/js/2.5e9520d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76eb001c96fda556ccfa",
+    "url": "/redux-movieinfo/static/js/main.62c7a1ba.chunk.js"
+  },
+  {
+    "revision": "0b1a02d674185c628127",
+    "url": "/redux-movieinfo/static/js/runtime-main.e55798fd.js"
+  }
+]);
